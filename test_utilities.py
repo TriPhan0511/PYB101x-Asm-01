b1f713b1f4618d76665990794825b55b5f2875e2
Tri Phan <phanphutri@gmail.com>
2023-05-10 22:12:01 +0700

    Updated

--- test_utilities.py
+++ test_utilities.py
@@ -1,16 +1,18 @@
 import math
 
-from trigonometric_sample import *
+from utilities import *
 
 # # Test case
 # cosine = 0.5
 # print(f'{round(compute_degree_from_cosine(cosine))}')  # 60 (degree)
 
 # # Test case 1: get_number function
+# Test function nhập một số từ bàn phím
 # num = get_number('Enter a number: ')
 # print(f'The entered number: {num}')
 
 # Test case 2: distance function
+# Test function tính khoảng cách giữa hai điểm
 # ax = 1
 # ay = 2
 # bx = 5
@@ -24,6 +26,7 @@ from trigonometric_sample import *
 # print(f'{distance(ax, ay, bx, by)}')
 
 # # Test case 3: is_triangle function
+# Test function xác thực 3 điểm cho trước có tao nên một tam giác
 # # A(6, -2) B(4, -6) C(12, -6): True
 # ax = 6
 # ay = -2
@@ -43,10 +46,12 @@ from trigonometric_sample import *
 # print(f'Three points A(6, 2) B(4, 6) C(4, 6): {is_triangle(ax, ay, bx, by, cx, cy)}')  # False
 
 # # Test case 4: compute_degree_from_cosine function
+# Test function tính số đo độ của một góc dựa trên cosine của góc đó
 # cosine = 0.5
 # print(f'{round(compute_degree_from_cosine(cosine))}')  # 60 (degree)
 
 # # Test case 5: create_vector function
+# Test function tạo ra một vector từ các tọa độ (x,y) của hai điểm
 # # A(6, -2) B(4, -6)
 # ax = 6
 # ay = -2
@@ -55,6 +60,7 @@ from trigonometric_sample import *
 # print(f'{create_vector(ax, ay, bx, by)}')  # [2, 4]
 
 # # Test case 6: compute_angle function
+# Test function tính số đo độ của một góc dựa trên các tọa độ (x,y) của 3 điểm
 # # A(16, -2) B(16, -6) C(18, -4)
 # ax = 16
 # ay = -2
@@ -65,6 +71,7 @@ from trigonometric_sample import *
 # print(compute_angle(ax, ay, bx, by, cx, cy))
 
 # Test case 7: compute three angles
+# Test tính số đo độ của 3 góc dựa trên các tọa độ (x,y) của 3 điểm
 # Nhập tọa độ 3 điểm từ bàn phím
 # A(16, -2) B(16, -6) C(18, -4)
 # ax = 16
@@ -91,6 +98,7 @@ from trigonometric_sample import *
 #     print(f'Góc BCA = {round(bca_angle)} (độ)')
 
 # Test case 8: is_right_triangle
+# Test function xác thực tam giác vuông
 # # A(4, -8) B(4, -12) C(10, -12): vuông tại đỉnh B
 # ax = 4
 # ay = -8
@@ -118,6 +126,7 @@ from trigonometric_sample import *
 #     print('Không phải làm tam giác vuông')
 
 # Test case 9 : is_obtuse_triangle function
+# Test function xác thực tam giác tù
 # # A(14, -8) B(10, -10) C(20, -10) : Tam giác tù tại đỉnh A
 # ax = 14
 # ay = -8
@@ -145,18 +154,14 @@ from trigonometric_sample import *
 #     print('Không phải làm tam giác tù')
 
 # Test case 10 : is_isosceles_triangle function
-# # A(16, -2) B(16, -6) C(18, -4) : Tam giác cân tại đỉnh C
-# ax = 16
-# ay = -2
-# bx = 16
-# by = -6
-# cx = 18
-# cy = -4
-# test_is_isosceles_triangle = is_isosceles_triangle(ax, ay, bx, by, cx, cy)
-# if test_is_isosceles_triangle[0]:
-#     print(f'Tam giác cân tại đỉnh {test_is_isosceles_triangle[1]}')
-# else:
-#     print('Không phải làm tam giác cân')
+# Test function xác thực tam giác cân
+# # A(8,3) B(7,0) C(9,0) : Tam giác cân tại đỉnh A
+# ax = 8
+# ay = 3
+# bx = 7
+# by = 0
+# cx = 9
+# cy = 0
 
 # # A(6, -2) B(4, -6) C(12, -6) : Không phải là tam giác cân
 # ax = 6
@@ -165,14 +170,16 @@ from trigonometric_sample import *
 # by = -6
 # cx = 12
 # cy = -6
+
 # test_is_isosceles_triangle = is_isosceles_triangle(ax, ay, bx, by, cx, cy)
 # if test_is_isosceles_triangle[0]:
 #     print(f'Tam giác cân tại đỉnh {test_is_isosceles_triangle[1]}')
 # else:
-#     print('Không phải là tam giác cân')
+#     print('Không phải làm tam giác cân')
+
 
 # Test case 11 : is_equilateral_triangle function
-
+# Test function xác thực tam giác đều
 # # A(math.sqrt(3), 2) B(0, 1) C(0, 3) : Tam giác đều
 # ax = math.sqrt(3)
 # ay = 2
@@ -180,24 +187,126 @@ from trigonometric_sample import *
 # by = 1
 # cx = 0
 # cy = 3
-# # test_is_equilateral_triangle = is_equilateral_triangle(ax, ay, bx, by, cx, cy)
-# if is_equilateral_triangle(ax, ay, bx, by, cx, cy):
+
+# # A(6, -2) B(4, -6) C(12, -6) : Không phải là tam giác đều
+# ax = 6
+# ay = -2
+# bx = 4
+# by = -6
+# cx = 12
+# cy = -6
+#
+# test_is_equilateral_triangle = is_equilateral_triangle(ax, ay, bx, by, cx, cy)
+# if test_is_equilateral_triangle[0]:
 #     print('Tam giác đều')
 # else:
 #     print('Không phải là tam giác đều')
 
-# A(6, -2) B(4, -6) C(12, -6) : Không phải là tam giác đều
-ax = 6
-ay = -2
-bx = 4
-by = -6
-cx = 12
-cy = -6
-# test_is_equilateral_triangle = is_equilateral_triangle(ax, ay, bx, by, cx, cy)
-if is_equilateral_triangle(ax, ay, bx, by, cx, cy):
-    print('Tam giác đều')
+# Test case 12 : is_right_and_isosceles_triangle function
+# Test function xác thực tam giác vuông cân
+# # A(1, 3) B(1, 1) C(3, 1) : Tam giác vuông cân tại đỉnh B
+# ax = 1
+# ay = 3
+# bx = 1
+# by = 1
+# cx = 3
+# cy = 1
+
+# # A(1, 4) B(1, 1) C(3, 1): Không phải là tam giác vuông cân (chỉ là tam giác vuông tại B)
+# ax = 1
+# ay = 4
+# bx = 1
+# by = 1
+# cx = 3
+# cy = 1
+
+# # A(8,3) B(7,0) C(9,0) : Không phải là tam giác vuông cân (chỉ là tam giác cân tại A)
+# ax = 8
+# ay = 3
+# bx = 7
+# by = 0
+# cx = 9
+# cy = 0
+
+# # A(5,4) B(4,2) C(5,1) : Không phải là tam giác vuông cân (chỉ là tam giác thường)
+# ax = 5
+# ay = 4
+# bx = 4
+# by = 2
+# cx = 5
+# cy = 1
+
+# test_is_right_and_isosceles_triangle = is_right_and_isosceles_triangle(ax, ay, bx, by, cx, cy)
+# if test_is_right_and_isosceles_triangle[0]:
+#     print(f'Tam giác vuông cân tại đỉnh {test_is_right_and_isosceles_triangle[1]}')
+# else:
+#     print('Không phải là tam giác vuông cân')
+
+# Test case 13 : is_obtuse_and_isosceles_triangle function
+# Test function xác thực tam giác tù cân
+# # A(6,2) B(3,1) C(9, 1) : Tam giác tù cân tại đỉnh A
+# ax = 6
+# ay = 2
+# bx = 3
+# by = 1
+# cx = 9
+# cy = 1
+
+# # A(6,2) B(3,1) C(9, 1) : Không phải là tam giác tù cân (chỉ là tam giác tù tại A)
+# ax = 6
+# ay = 2
+# bx = 3
+# by = 1
+# cx = 10
+# cy = 1
+
+# # A(2,4) B(1,1) C(3, 1) : Không phải là tam giác tù cân (chỉ là tam giác cân tại A)
+# ax = 2
+# ay = 4
+# bx = 1
+# by = 1
+# cx = 3
+# cy = 1
+
+# # A(4,4) B(2,2) C(3, 1) : Không phải là tam giác tù cân (chỉ là tam giác thường)
+# ax = 4
+# ay = 4
+# bx = 2
+# by = 2
+# cx = 3
+# cy = 1
+
+# test_is_obtuse_and_isosceles_triangle = is_obtuse_and_isosceles_triangle(ax, ay, bx, by, cx, cy)
+# if test_is_obtuse_and_isosceles_triangle[0]:
+#     print(f'Tam giác tù tại đỉnh A và cân tại đỉnh {test_is_obtuse_and_isosceles_triangle[1]}')
+# else:
+#     print('Không phải là tam giác tù cân')
+
+# Test case 14 : is_normal_triangle function
+# Test function xác thực tam giác thường
+# A(4,4) B(2,2) C(3, 1) : Tam giác thường
+# ax = 4
+# ay = 4
+# bx = 2
+# by = 2
+# cx = 3
+# cy = 1
+
+ax = 4
+ay = 3
+bx = 3
+by = 1
+cx = 6
+cy = 1
+
+if is_normal_triangle(ax, ay, bx, by, cx, cy)[0]:
+    print('Tam giác thường')
 else:
-    print('Không phải là tam giác đều')
+    print('Không phải tam giác thường')
+
+
+
+
 
 
 
---
+++ utilities.py
@@ -0,0 +1,202 @@
+from math import *
+
+
+# Hàm nhập một số từ bàn phím
+def get_number(message):
+    while True:
+        inp = input(message)
+        try:
+            return int(inp)
+        except ValueError:
+            print('Sai định dạng. Vui lòng nhập vào một số!')
+            continue
+
+
+# Hàm tính khoảng cách giữa hai điểm dựa trên các tọa độ (x,y) của hai điểm đó
+def compute_distance(ax, ay, bx, by):
+    return round(sqrt((bx - ax) ** 2 + (by - ay) ** 2), 2)
+
+
+# Hàm xác định ba điểm có thể tạo nên một tam giác được không dựa trên các tọa độ (x,y) của ba điểm đó
+def is_triangle(ax, ay, bx, by, cx, cy):
+    d1 = compute_distance(ax, ay, bx, by)
+    d2 = compute_distance(ax, ay, cx, cy)
+    d3 = compute_distance(bx, by, cx, cy)
+    return d1 + d2 > d3 and d1 + d3 > d2 and d2 + d3 > d1
+
+
+# # Hàm tính độ của góc dựa trên cosine của góc đó
+# def compute_degree_from_cosine(cosine):
+#     rad = acos(cosine)
+#     deg = degrees(rad)
+#     return deg
+
+
+# # Hàm tạo nên một vector dựa trên các tọa độ (x,y) của hai điểm
+# def create_vector(ax, ay, bx, by):
+#     return [(ax - bx), (ay - by)]
+
+
+# # Hàm tính độ của góc được tạo nên bởi hai vector
+# # Đầu vào là các tọa độ (x,y) của ba điểm.
+# def compute_angle(ax, ay, bx, by, cx, cy):
+#     vector1 = create_vector(ax, ay, bx, by)
+#     vector2 = create_vector(bx, by, cx, cy)
+#     # dividend = abs(vector1[0]*vector2[0] + vector1[1]*vector2[1])
+#     dividend = vector1[0]*vector2[0] + vector1[1]*vector2[1]
+#     divisor = sqrt(vector1[0]**2 + vector1[1]**2) * sqrt(vector2[0]**2 + vector2[1]**2)
+#     if divisor != 0:
+#         cosine = dividend / divisor
+#         return round(compute_degree_from_cosine(cosine))
+
+
+# Hàm tính độ của một góc trong tam giác
+# Đầu vào là các tọa độ (x,y) của ba điểm.
+def compute_angle_in_a_triangle(ax, ay, bx, by, cx, cy):
+    ang = degrees(atan2(cy - by, cx - bx) - atan2(ay - by, ax - bx))
+    ang = ang + 360 if ang < 0 else ang
+    return round(360 - ang if ang > 180 else ang, 2)
+
+
+# Hàm xác định tam giác vuông
+# Trong tam giác có 1 góc = 90 độ.
+def is_right_triangle(ax, ay, bx, by, cx, cy):
+    if is_triangle(ax, ay, bx, by, cx, cy):
+        angle_a = compute_angle_in_a_triangle(bx, by, ax, ay, cx, cy)
+        angle_b = compute_angle_in_a_triangle(ax, ay, bx, by, cx, cy)
+        angle_c = compute_angle_in_a_triangle(ax, ay, cx, cy, bx, by)
+        print(f'DEBUG: angle_a: {angle_a}')
+        print(f'DEBUG: angle_b: {angle_b}')
+        print(f'DEBUG: angle_c: {angle_c}')
+        if angle_a == 90:
+            return [True, 'A']
+        if angle_b == 90:
+            return [True, 'B']
+        if angle_c == 90:
+            return [True, 'B']
+    return [False]
+
+
+# Hàm xác định tam giác tù
+# Trong tam giác có 1 góc > 90 độ.
+def is_obtuse_triangle(ax, ay, bx, by, cx, cy):
+    if is_triangle(ax, ay, bx, by, cx, cy):
+        angle_a = compute_angle_in_a_triangle(bx, by, ax, ay, cx, cy)
+        angle_b = compute_angle_in_a_triangle(ax, ay, bx, by, cx, cy)
+        angle_c = compute_angle_in_a_triangle(ax, ay, cx, cy, bx, by)
+        if angle_a > 90:
+            return [True, 'A']
+        if angle_b > 90:
+            return [True, 'B']
+        if angle_c > 90:
+            return [True, 'B']
+    return [False]
+
+
+# Hàm xác định tam giác cân
+# Trong tam giác có 2 cạnh bằng nhau
+def is_isosceles_triangle(ax, ay, bx, by, cx, cy):
+    if is_triangle(ax, ay, bx, by, cx, cy):
+        ab = compute_distance(ax, ay, bx, by)
+        ac = compute_distance(ax, ay, cx, cy)
+        bc = compute_distance(bx, by, cx, cy)
+        if ab == ac:
+            return [True, 'A']
+        if ab == bc:
+            return [True, 'B']
+        if ac == bc:
+            return [True, 'C']
+    return [False]
+
+
+# Hàm xác định tam giác đều
+# Tam giác có 3 cạnh bằng nhau hoặc 3 góc bằng 60 độ.
+def is_equilateral_triangle(ax, ay, bx, by, cx, cy):
+    if is_triangle(ax, ay, bx, by, cx, cy):
+        ab = compute_distance(ax, ay, bx, by)
+        ac = compute_distance(ax, ay, cx, cy)
+        bc = compute_distance(bx, by, cx, cy)
+        if ab == ac == bc:
+            return [True]
+    return [False]
+
+
+# Hàm xác định tam giác vuông cân
+# Đảm bảo cả tiêu chí của tam giác vuông và tam giác cân.
+def is_right_and_isosceles_triangle(ax, ay, bx, by, cx, cy):
+    right_triangle = is_right_triangle(ax, ay, bx, by, cx, cy)
+    isosceles_triangle = is_isosceles_triangle(ax, ay, bx, by, cx, cy)
+    print(f'DEBUG: right_triangle[0]: {right_triangle[0]}')
+    print(f'DEBUG: isosceles_triangle[0]: {isosceles_triangle[0]}')
+    if right_triangle[0] and isosceles_triangle[0]:
+        print(f'DEBUG: HERE')
+        return [True, right_triangle[1]]
+    return [False]
+
+
+def is_obtuse_and_isosceles_triangle(ax, ay, bx, by, cx, cy):
+    obtuse_triangle = is_obtuse_triangle(ax, ay, bx, by, cx, cy)
+    isosceles_triangle = is_isosceles_triangle(ax, ay, bx, by, cx, cy)
+    if obtuse_triangle[0]:
+        print(f'DEBUG: obtuse_triangle[0]: {obtuse_triangle[0]} obtuse_triangle[1]: {obtuse_triangle[1]}')
+    else:
+        print(f'DEBUG: obtuse_triangle[0]: {obtuse_triangle[0]}')
+    if isosceles_triangle[0]:
+        print(f'DEBUG: isosceles_triangle[0]: {isosceles_triangle[0]} isosceles_triangle[1]: {isosceles_triangle[1]}')
+    else:
+        print(f'DEBUG: isosceles_triangle[0]: {isosceles_triangle[0]}')
+    if obtuse_triangle[0] and isosceles_triangle[0]:
+        return [True, obtuse_triangle[1]]
+    return [False]
+
+
+def is_normal_triangle(ax, ay, bx, by, cx, cy):
+    if not is_triangle(ax, ay, bx, by, cx, cy):
+        return [False]
+    right_triangle = is_right_triangle(ax, ay, bx, by, cx, cy)
+    obtuse_triangle = is_obtuse_triangle(ax, ay, bx, by, cx, cy)
+    isosceles_triangle = is_isosceles_triangle(ax, ay, bx, by, cx, cy)
+    equilateral_triangle = is_equilateral_triangle(ax, ay, bx, by, cx, cy)
+    if right_triangle[0] or obtuse_triangle[0] or isosceles_triangle[0] or equilateral_triangle[0]:
+        print(f'DEBUG: right_triangle[0]: {right_triangle[0]}')
+        print(f'DEBUG: obtuse_triangle[0]: {obtuse_triangle[0]}')
+        print(f'DEBUG: isosceles_triangle[0]: {isosceles_triangle[0]}')
+        print(f'DEBUG: equilateral_triangle[0]: {equilateral_triangle[0]}')
+        return [False]
+    return [True]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
